42513d71c65bcd5e0441b0ac6fa6ccbf6c71138f
Inspira <Inspira@inspiras-mac-mini.local>
2018-09-26 13:00:52 +0530

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Inspira on 26/09/18.
+//  Copyright © 2018 myself. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
