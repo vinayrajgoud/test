1f30137b3ad6a693a0c066c9131a116513cf8e3c
Inspira <Inspira@inspiras-mac-mini.local>
2018-09-26 13:10:24 +0530

    new line

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //git test2
+        // new line
         // Do any additional setup after loading the view, typically from a nib.
     }
 
